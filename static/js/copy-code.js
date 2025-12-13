829d7188acfa05bb5eb800988701ea08596cb17b
crnh <30109443+crnh@users.noreply.github.com>
2025-12-13 13:51:03 +0100

    JS: Add copy code button to parent div instead of pre

--- static/js/copy-code.js
+++ static/js/copy-code.js
@@ -7,8 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     copyButton.className = "copy-code-button";
     copyButton.textContent = "copy";
 
-    // Insert the button inside the <pre> block
-    codeBlock.appendChild(copyButton);
+    // Insert the button inside the <div> block
+    const codeDiv = codeBlock.parentElement;
+    codeDiv.appendChild(copyButton);
 
     copyButton.addEventListener("click", function () {
       const code = codeBlock.querySelector("code");
